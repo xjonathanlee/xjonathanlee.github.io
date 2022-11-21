98e985901806b98b0c72da8b320f6735b36b897c
JONATHAN LEE <jon.leesc2@gmail.com>
2022-11-21 13:49:27 -0800

    updated a couple of things

--- js/randomorder.js
+++ js/randomorder.js
@@ -1,6 +1,6 @@
 
 //i want all columns to be of equal length after shuffling.
-
+//this is something you can work on for 2022
 var cards = $(".grid-item");
 for(var i = 0; i < cards.length; i++){
     var target = Math.floor(Math.random() * cards.length -1) + 1;
