0ee01b71c5a5f20e52269d5c7d4da53b503ce3e1
JONATHAN LEE <jon.leesc2@gmail.com>
2022-12-05 17:01:56 -0800

    update footers and capitilization

--- js/backgroundchange.js
+++ js/backgroundchange.js
@@ -1,15 +1,2 @@
-console.log("init");
-
-
-const elements = document.querySelectorAll('.titleProject');
-elements.forEach(elem =>{
-	elem.addEventListener("mouseenter",function(event){
-		const bg = document.querySelector('.backchange')
-		const color = event.target.getAttribute("data-color");
-		bg.style.backgroundColor= color;
-	},false)
-});
-
-document.getElementById("bchange").style.transition ="all .5s"
-
+console.log("init");const elements=document.querySelectorAll(".titleProject");elements.forEach(e=>{e.addEventListener("mouseenter",function(e){let t=document.querySelector(".backchange"),l=e.target.getAttribute("data-color");t.style.backgroundColor=l},!1)}),document.getElementById("bchange").style.transition="all .5s";
 /*https://stackoverflow.com/questions/52982780/change-body-background-color-on-hover*/
---
+++ js/okhover.js
@@ -0,0 +1,2 @@
+// For The intropage, doesnt work beyond the first "real page" of the website
+/*depreciated*/
