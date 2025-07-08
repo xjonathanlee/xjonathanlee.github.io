9957963e8ac4014788dcaf22c53d73ddc0216197
xjonathanlee <jklmdia@gmail.com>
2025-07-08 00:40:46 -0700

    added 1 graphic

--- js/greetings.js
+++ js/greetings.js
@@ -1,1 +1,4 @@
-const greetings = ["Gutentag! I am Jon.","Hello! I am Jon.","Bonjour! I am Jon.","Howdy! I am Jon.","こんいちわ! I am Jon.","Salut! I am Jon.","Hei! I am Jon.","Hjei! I am Jon.","Saelt! I am Jon.","Hola! I am Jon.","Tudo Bem! I am Jon."," Benvingudes! I am Jon.","Bon Dia! I am Jon.","Hej! I am Jon.","G'Day! I am Jon."]
+const greetings = ["Gutentag! I am Jon.","Hello! I am Jon.","Komdu blessað(ur)! I am Jon.","Howdy! I am Jon.","こんいちわ! I am Jon.","Salut! I am Jon.","Hei! I am Jon.","Hjei! I am Jon.","Hola! I am Jon.","Tudo Bem! I am Jon."," Benvingudes! I am Jon.","Bon Dia! I am Jon.","Hej! I am Jon.","哈囉! I am Jon."];
+
+const randomIndex = Math.floor(Math.random()*greetings.length);
+document.getElementById("greet").innerText=greetings[randomIndex];
